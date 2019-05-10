be5d645b3dad6f77a998658fd7dfd6eadfcb9acd
Thomas the Franque <tfranque@gmail.com>
2019-05-10 22:53:57 +0100

    Initial files

---
+++ ZombieGame/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ZombieGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("\t When i'm big, I want to be a game! \n" +
+                "\t - Said small program.cs ");
+            Console.ResetColor();
+        }
+    }
+}
