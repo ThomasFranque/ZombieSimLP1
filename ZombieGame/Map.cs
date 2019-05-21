eb337ffa5f2c15addd0f04f3688e7a298f6c2cf5
Miguel Romão Fernández <miguelrf95@gmail.com>
2019-05-21 14:13:18 +0100

    Map now prints in the expected format, needs to be completed still

--- ZombieGame/Map.cs
+++ ZombieGame/Map.cs
@@ -16,8 +16,13 @@ namespace ZombieGame
         public void ShowMap(int x, int y, int h, int z, int pH, int pZ)
         {
             for (int i = 0; i < x; i++)
+            {
                 for (int j = 0; j < y; j++)
-                    Console.WriteLine("map");
+                {
+                    Console.Write(" map ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
